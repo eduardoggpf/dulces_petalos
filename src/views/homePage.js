396ce25affc389add81b717ddf78a3471366988e
Eduardo Gomez <egomezgiron@gmail.com>
2024-03-18 16:53:45 +0100

    changes call

--- src/views/homePage.js
+++ src/views/homePage.js
@@ -3,10 +3,10 @@ import ListComponent from '../components/listComponent';
 
 function Home() {
   return (
-    <div className="Home">
-      <HeaderComponent></HeaderComponent>
-      <ListComponent></ListComponent>
-    </div>
+    <>
+      <HeaderComponent/>
+      <ListComponent/>
+    </>
   );
 }
 
